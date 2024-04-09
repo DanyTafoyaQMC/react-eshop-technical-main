c6026d04f51601c7907f840bf47f6dd410dfba9d
DanyTafoyaQMC <edtafoya@qmcmex.com>
2024-04-09 12:51:53 -0600

    now you can remove from cart

--- src/eshop/components/CartItem.jsx
+++ src/eshop/components/CartItem.jsx
@@ -1,15 +1,19 @@
 import { useEffect } from "react";
 import { useFetchProducts } from "../hooks/useFetchProducts";
 import { Loading } from "./Loading";
+import { useCart } from "../hooks/useCart";
 
 
 // eslint-disable-next-line react/prop-types
 export const CartItem = ({ id }) => {
     const { product, isLoading, fetchProduct } = useFetchProducts();
-    const { name, imageUrl: img, quantity } = product;
+    const { handleRemoveFromCart } = useCart();
+    const { name, imageUrl: img, price} = product;
     useEffect(()=>{
         fetchProduct(id)
     },[id]);
+
+
   return (
     <>
         { isLoading ? <Loading /> : (
@@ -21,12 +25,14 @@ export const CartItem = ({ id }) => {
                   <div className="col-md-8">
                     <div className="card-body">
                       <h5 className="card-title">{name}</h5>
-                      <p className="card-text">Quantity: {quantity}</p>
+                      <p className="card-text">{price}</p>
+                      <p className="card-text">Quantity:</p>
                       <div className="btn-group" role="group" aria-label="Quantity">
                         <button type="button" className="btn btn-secondary">-</button>
                         <button type="button" className="btn btn-secondary">+</button>
                       </div>
-                      <button type="button" className="btn btn-danger">Remove</button>
+                      <button type="button" className="btn btn-danger" onClick={ ()=>handleRemoveFromCart(id)}>Remove</button>
+                      <button type="button" className="btn btn-success float-end">Buy</button>
                     </div>
                   </div>
                 </div>
---
+++ src/eshop/hooks/useCart.js
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { CartContext } from "../context/Cart";
+
+
+export const useCart = () => {
+    const { state, dispatch } = useContext(CartContext);
+
+    const handleAddToCart = (id) => {
+      dispatch({ type: "ADD_TO_CART", payload: { id } });
+    };
+
+    const handleRemoveFromCart = (id) => {
+      dispatch({ type: "REMOVE_FROM_CART", payload: { id } });
+    };
+    const addQuantity = (id) => {
+      dispatch({ type: "ADD_QUANTITY", payload: { id } });
+    };
+  return {
+    cart: state.cart,
+    handleAddToCart,
+    handleRemoveFromCart,
+    addQuantity,
+  }
+}
